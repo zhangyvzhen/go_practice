1896a8b759b75f5e2b337b34e0e6b6bee2df674e
zyz <41181614@qq.com>
2024-09-30 04:26:58 +0800

    di
    解决参数未使用的问题, 可以用 _ 忽略

--- internal/di/di.go
+++ internal/di/di.go
@@ -20,6 +20,6 @@ func Greet(writer io.Writer, name string) {
 	// fmt.Fprintf 和 fmt.Printf 一样，只不过 fmt.Fprintf 会接收一个 Writer 参数，用于把字符串传递过去，而 fmt.Printf 默认是标准输出。
 }
 
-func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+func MyGreeterHandler(w http.ResponseWriter, _ *http.Request) {
 	Greet(w, "world")
 }
