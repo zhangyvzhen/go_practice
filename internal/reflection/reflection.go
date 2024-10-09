202c2a7556cfd22a2dbedd5fc37f4f58eb758442
zyz <41181614@qq.com>
2024-10-10 02:34:45 +0800

    reflection
    使用递归遍历任意数据结构

---
+++ internal/reflection/reflection.go
@@ -0,0 +1,41 @@
+package reflection
+
+import "reflect"
+
+// golang 挑战：编写函数 walk(x interface{}, fn func(string))，
+// 参数为结构体 x，并对 x 中的所有字符串字段调用 fn 函数。难度级别：递归。
+func walk(x interface{}, fn func(input string)) {
+	val := getValue(x)
+
+	walkValue := func(value reflect.Value) {
+		walk(value.Interface(), fn)
+	}
+
+	switch val.Kind() {
+	case reflect.String:
+		fn(val.String())
+
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walkValue(val.Field(i))
+		}
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < val.Len(); i++ {
+			walkValue(val.Index(i))
+		}
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			walkValue(val.MapIndex(key))
+		}
+	}
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+
+	return val
+}
